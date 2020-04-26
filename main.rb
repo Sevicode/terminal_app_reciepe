103e632445678d94709d26fea8d4b2e0982e8f32
Sevda Amini <sevda.amini@hotmail.com>
2020-04-26 21:38:11 +1000

    README file created

--- main.rb
+++ main.rb
@@ -3,21 +3,19 @@ require_relative('./lib/menus')
 
 require_relative('./lib/reciepes')
 
-
+#-------this is the main page for running the app / Main class starts here------
 class Main 
 attr_accessor :main_menu 
      
     def initialize 
       @main_menu = main_menu
       @main_menu = "salad" "sandwich", "cake"
-     
-      
      end
+
 
      system "clear"
 
-
-     #----------Asking the user about their selection----------------------------
+#----------Main menu user iquiry using TTY Prompt----------------------------
 
      menu_options = %w[Salad Sandwich Cake]
      prompt = TTY::Prompt.new
@@ -25,14 +23,13 @@ attr_accessor :main_menu
 
      system "clear"
 
-    #---------------------------------------------------------------------
+#------------Using reciepe.rb amd menus.rb starts here------------------------
     
         display_menu = Menus.new
         reciepe_list = Reciepes.new
 
-    #----CONDITIONALS ---------
 
-    #---------displaying user's salad reciepe----------------
+#---------displaying user's salad reciepe----------------
 
     if selection == "Salad"
         
@@ -49,28 +46,28 @@ attr_accessor :main_menu
                 puts reciepe_list.salad_list["Fruit salad"][1] 
                 
 
-                elsif salad_selection == "Potato"
+            elsif salad_selection == "Potato"
 
                 puts "This is the ingredients for #{salad_selection}"
                 puts reciepe_list.salad_list["Potato salad"][0] 
                 puts "and this is the instruction! Enjoy!"
                 puts reciepe_list.salad_list["Green salad"][1] 
 
-                    elsif salad_selection == "Green"
+            elsif salad_selection == "Green"
 
-                    puts "This is the ingredients for #{salad_selection}"
-                    puts reciepe_list.salad_list["Green salad"][0] 
-                    puts "and this is the instruction! Enjoy!"
-                    puts reciepe_list.salad_list["Green salad"][1] 
+                puts "This is the ingredients for #{salad_selection}"
+                puts reciepe_list.salad_list["Green salad"][0] 
+                puts "and this is the instruction! Enjoy!"
+                puts reciepe_list.salad_list["Green salad"][1] 
 
-                    else
-                        puts "Sorry! this reciepe does not exist!"
+            else
+                puts "Sorry! this reciepe does not exist!"
                         
 
             end
 
            
-        #---------displaying user's requested sandwich reciepe ----
+        #---------displaying user's requested SANDWICH reciepe ----
 
     elsif selection == "Sandwich"
 
@@ -86,28 +83,28 @@ attr_accessor :main_menu
                 puts reciepe_list.sandwich_list["Eggs sandwich"][1] 
 
                 
-                elsif sandwich_selection == "Ham"
+            elsif sandwich_selection == "Ham"
 
-                    puts "This is the ingredients for #{sandwich_selection}"
-                    puts reciepe_list.sandwich_list["Ham sandwich"][0] 
-                    puts "and this is the instruction! Enjoy!"
-                    puts reciepe_list.sandwich_list["Ham sandwich"][1]
+                puts "This is the ingredients for #{sandwich_selection}"
+                puts reciepe_list.sandwich_list["Ham sandwich"][0] 
+                puts "and this is the instruction! Enjoy!"
+                puts reciepe_list.sandwich_list["Ham sandwich"][1]
 
-                    elsif sandwich_selection == "Chicken"
+            elsif sandwich_selection == "Chicken"
 
-                        puts "This is the ingredients for #{sandwich_selection}"
-                        puts reciepe_list.sandwich_list["Chicken sandwich"][0] 
-                        puts "and this is the instruction! Enjoy!"
-                        puts reciepe_list.sandwich_list["Chicken sandwich"][1]
+                puts "This is the ingredients for #{sandwich_selection}"
+                puts reciepe_list.sandwich_list["Chicken sandwich"][0] 
+                puts "and this is the instruction! Enjoy!"
+                puts reciepe_list.sandwich_list["Chicken sandwich"][1]
 
-                    else
+            else
 
-                        puts "Sorry! this reciepe does not exist!"
+                puts "Sorry! this reciepe does not exist!"
 
             end
 
     
-        #---------displaying user's requested cake reciepe --------
+        #---------displaying user's requested CAKE reciepe --------
 
     elsif selection == "Cake"
             cake_menu = %w[Sponge Carrot Bannana]
@@ -115,19 +112,19 @@ attr_accessor :main_menu
             cake_selection = prompt.select("mmm a cake!...what sort of cake?!", cake_menu)
 
             if cake_selection == "Sponge"
-                puts "This is the ingredients for #{cake_selection}"
+                puts "This is the ingredients for #{cake_selection} cake!"
                 puts reciepe_list.cake_list["Sponge cake"][0] 
                 puts "and this is the instruction! Enjoy!"
                 puts reciepe_list.cake_list["Sponge cake"][1]
                 
             elsif cake_selection == "Carrot"
-                puts "This is the ingredients for #{cake_selection}"
+                puts "This is the ingredients for #{cake_selection} cake!"
                 puts reciepe_list.cake_list["Carrot cake"][0] 
                 puts "and this is the instruction! Enjoy!"
                 puts reciepe_list.cake_list["Carrot cake"][1]
 
             elsif cake_selection == "Bannana"
-                puts "This is the ingredients for #{cake_selection}"
+                puts "This is the ingredients for #{cake_selection} cake!"
                 puts reciepe_list.cake_list["Bannana cake"][0] 
                 puts "and this is the instruction! Enjoy!"
                 puts reciepe_list.cake_list["Bannana cake"][1]
